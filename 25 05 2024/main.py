ba77e54d672acd0cdf5935f5eb079af8234b7bb4
brunoxing2004 <mail@brunosilva.co.uk>
2024-05-25 10:24:32 +0100

    updateLavagem e criaçãoLavagem

--- 25 05 2024/main.py
+++ 25 05 2024/main.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from datetime import datetime
 
 # DB connection
 conn = mysql.connector.connect(
@@ -20,7 +21,31 @@ cursor = conn.cursor()
 # Close connection
 conn.close()
 
-def updateDados():
-  print("Hello from a function")
 
-my_function()
+def criacaoLavagem(matricula, data_lavagem, tipo, estado):
+    query = """
+    INSERT INTO lavagem (matricula, data_lavagem, tipo, estado)
+    VALUES (%s, %s, %s, %s)
+    """
+
+    #matricula = "92-22-MS"
+    #data_lavagem = datetime(2024, 5, 25, 11, 30)
+    #tipo = "Detalhado"
+    #estado = "Agendado"
+    valores = (matricula, data_lavagem, tipo, estado)
+    cursor.execute(query, valores)
+    conn.close()
+
+
+def updateLavagem(id, matricula, data_lavagem, tipo, estado):
+    query = """
+    UPDATE lavagem
+    SET matricula = %s, data_lavagem = %s, tipo = %s, estado = %s
+    WHERE id = %s
+    """
+    valores = (id, matricula, data_lavagem, tipo, estado)
+    cursor.execute(query, valores)
+    conn.close()
+
+updateLavagem(1, "92-22-MS", datetime(2024, 5, 25, 11, 30), "Detalhada", "Agendado")
+print("Ok, update")
