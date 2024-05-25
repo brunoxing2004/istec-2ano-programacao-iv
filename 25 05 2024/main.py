e4c175be30cb24a9af56ad973a4b21cc72ff2a5c
brunoxing2004 <mail@brunosilva.co.uk>
2024-05-25 10:04:15 +0100

    Update main.py

--- 25 05 2024/main.py
+++ 25 05 2024/main.py
@@ -10,11 +10,17 @@ database="programacao-istec"
 cursor = conn.cursor()
 
 # Execute query
-cursor.execute("SELECT * FROM teste")
+#cursor.execute("SELECT * FROM lavagem")
 
 # Get data
-results = cursor.fetchall()
-for line in results:
-    print(line)
+#results = cursor.fetchall()
+#for line in results:
+#    print(line)
+
 # Close connection
 conn.close()
+
+def updateDados():
+  print("Hello from a function")
+
+my_function()
